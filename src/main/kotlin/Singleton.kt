bdc5b5e22f0e37ee3225948a328542e60c3598f5
hieudd-teko <hieu.dd@teko.vn>
2021-02-23 18:27:20 +0700

    + Singleton

---
+++ src/main/kotlin/Singleton.kt
@@ -0,0 +1,11 @@
+class Singleton private constructor() {
+    companion object {
+        private var instance: Singleton? = null
+        fun getInstance(): Singleton {
+            if (instance == null) {
+                instance = Singleton()
+            }
+            return instance ?: throw Exception("LazyInitialization not init")
+        }
+    }
+}
